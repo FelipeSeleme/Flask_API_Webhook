64e04c89f78f9dc97cd87a4c098badfbb878856b
Felipe Seleme Ribeiro <felipeselemeribeiro@gmail.com>
2023-05-19 10:59:47 -0300

    Delete webhook.py

--- flaskwebsite/webhook.py
+++
@@ -1,8 +0,0 @@
-import requests
-import json
-
-webhook_url = 'http://127.0.0.1:5000/teste'
-
-data = '{"nome": "Devayanne2", "email": "Jayani06@yandex.com", "status": "aprovado", "valor": 520, "forma_pagamento": "pix", "parcelas": 4}'
-
-r = requests.post(webhook_url, data=json.dumps(data), headers={'Content-Type': 'application/json'})
